eba3985b96f223a9da6b911fcf1b2afbe06178da
alynch-code <athinelynch@icloud.com>
2020-07-07 23:47:35 -0500

    Add schema to app.js upate code to store posts in database and post to main page.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 const blogContents = "";
-const journal = [];
+// const journal = [];
 
 const app = express();
 const Schema = mongoose.Schema;
@@ -29,25 +29,24 @@ const postSchema = new Schema ({
 
 const Post = mongoose.model("Post", postSchema);
 
+
 app.get("/", function(req,res){
-  res.render("home",{
-    hContent: homeStartingContent,
-    pContent: journal
+  Post.find({}, function(error, posts){
+    res.render("home",{
+      hContent: homeStartingContent,
+      pContent: posts
+    })
   })
 });
 
-app.get("/posts/:title", function(req,res){
-  const routes = _.lowerCase(req.params.title)
+app.get("/posts/:id", function(req,res){
+  const postId = req.params.id
 
-  journal.forEach(function(post){
-    const postName = _.lowerCase(post.titles)
-
-    if (routes === postName) {
+  Post.findOne({_id:id}, function(error, post){
       res.render("post", {
-        pTitle: post.titles,
-        pPost: post.posts,
+        pTitle: post.title,
+        pPost: post.post,
       })
-    }
   })
 });
 
@@ -70,12 +69,15 @@ app.get("/compose", function(req,res){
 });
 
 app.post("/compose",function(req,res){
-  const blogInfo = new Post({
-    titles: req.body.title,
-    posts: req.body.post
+  const blogInfo = new Post ({
+    title: req.body.title,
+    post: req.body.post
   })
-  journal.push(blogInfo)
-  res.redirect("/")
+  blogInfo.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+  });
 });
 
 app.listen(process.env.PORT || 8080,function(){
